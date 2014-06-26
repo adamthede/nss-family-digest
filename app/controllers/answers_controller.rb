be6536557217db1edbe09c89d4c0d3ca6cdd8876
Adam Thede <adam@thedetech.com>
2014-06-25 21:34:37 -0500

    tinkering with postmark

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   skip_before_filter  :verify_authenticity_token
+  prepend_before_filter :require_no_authentication
 
   def create_from_inbound_hook
     message = params
