fcf4319160302989feda4776f91e1066c7c7d0ba
Adam Thede <adam@thedetech.com>
2014-06-26 09:10:26 -0500

    process inbound set to render ok

--- app/controllers/email_response_controller.rb
+++ app/controllers/email_response_controller.rb
@@ -3,7 +3,7 @@ class EmailResponseController < ApplicationController
   skip_before_action :authenticate_user!
 
   def create_from_inbound_hook
-
+    head :ok, :content_type => 'text/html'
   end
 
 end
