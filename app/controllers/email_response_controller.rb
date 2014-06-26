7ae41dcbe8d0d412346c35c32bd8c0a26426d98a
Adam Thede <adam@thedetech.com>
2014-06-25 23:11:20 -0500

    working to solve the internal server error mystery for the inbound hook

--- app/controllers/email_response_controller.rb
+++ app/controllers/email_response_controller.rb
@@ -1,5 +1,5 @@
 class EmailResponseController < ApplicationController
-  skip_before_filter  :verify_authenticity_token
+  # skip_before_filter  :verify_authenticity_token
   skip_before_action :authenticate_user!
 
   def create_from_inbound_hook
