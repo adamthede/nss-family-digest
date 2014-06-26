fdc3ed8297052c4a38b253c8ece1d82ef48f01ed
Adam Thede <adam@thedetech.com>
2014-06-26 10:21:48 -0500

    store incoming email to database

--- app/controllers/email_response_controller.rb
+++ app/controllers/email_response_controller.rb
@@ -3,6 +3,14 @@ class EmailResponseController < ApplicationController
   skip_before_action :authenticate_user!
 
   def create_from_inbound_hook
+    puts params
+    from = params[:From]
+    puts from
+    subject = params[:Subject]
+    puts subject
+    textbody = params[:TextBody]
+    puts textbody
+    Answer.create_from_email(from, subject, textbody)
     head :ok, :content_type => 'text/html'
   end
 
