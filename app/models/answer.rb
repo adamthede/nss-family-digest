8c61457094e85731d0f217c874edb2b114485597
Adam Thede <adam@thedetech.com>
2014-06-26 21:56:40 -0500

    minor mod for heroku deployment

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -20,10 +20,8 @@ class Answer < ActiveRecord::Base
       group_id = group.id.to_s
       question_record = QuestionRecord.where(:group_id => group_id, :question_id => question_id).last
       question_record_id = question_record.id.to_s
-      answer = textbody.split('On #{question_record.created_at.strf(%b %d, %Y)}, at ').first.strip
-      new_answer = answer.gsub(/Sent from my iPhone/,'').strip
       if question_record.created_at < 5.days.ago
-        Answer.create(:answer => new_answer,
+        Answer.create(:answer => textbody,
                       :user_id => user.id,
                       :question_records_id => question_record_id)
       else
