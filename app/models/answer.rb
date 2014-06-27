66dee02ba5dd8863d3b41be8dc30f3db538c3bb6
Adam Thede <adam@thedetech.com>
2014-06-26 22:11:42 -0500

    fixed logic allowing answers within a specific time range

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -20,12 +20,12 @@ class Answer < ActiveRecord::Base
       group_id = group.id.to_s
       question_record = QuestionRecord.where(:group_id => group_id, :question_id => question_id).last
       question_record_id = question_record.id.to_s
-      if question_record.created_at < 5.days.ago
+      if DateTime.now < question_record.created_at + 5.days
         Answer.create(:answer => textbody,
                       :user_id => user.id,
                       :question_records_id => question_record_id)
       else
-        return
+        logger.info("TOO LATE!  Answering has closed for this question.")
       end
     else
       logger.info("No user found with email: #{from.to_s}")
