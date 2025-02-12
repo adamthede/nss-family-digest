1fbe653b80bc9dd8e62039ff15c7382dd59b5d21
Adam Thede <adam@thedetech.com>
2025-02-11 21:06:17 -0600

    Remove legacy Tailwind CSS and application JavaScript files
    - Delete application.tailwind.css from stylesheets directory
    - Remove precompiled Tailwind and application JavaScript assets
    - Clean up public/assets directory of generated and redundant files
    - Simplify asset management and configuration

--- public/assets/controllers/dropdown_controller-563b73a0fa5ce6f18c54908e9e928f1853a259fbd29957349cee7c7a997fd461.js
+++ public/assets/controllers/dropdown_controller-563b73a0fa5ce6f18c54908e9e928f1853a259fbd29957349cee7c7a997fd461.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
+// Explicitly name the controller
+export default class DropdownController extends Controller {
   static targets = ["menu"]
 
   toggle(event) {
