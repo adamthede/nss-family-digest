a96a68e13e5e8dcf03303d3af2abbe3aa1830956
Adam Thede <adam@thedetech.com>
2025-02-19 07:32:27 -0600

    assets

--- public/assets/controllers/index-cd36b7012150fad856aca725babc5e33f22b332e11c83dcf831e2d243e1da341.js
+++
@@ -1,5 +0,0 @@
-import { application } from "../main"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-
-// Automatically load all controllers defined in the controllers folder
-eagerLoadControllersFrom("controllers", application);
