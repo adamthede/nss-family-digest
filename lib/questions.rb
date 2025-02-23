ef65f45f10b421e97f44ebb590d7156c645a3d30
Adam Thede <adam@thedetech.com>
2025-02-22 18:30:21 -0600

    Simplify Question Selection Logic in Group Interactions
    - Refactor question selection to use centralized method from Question model
    - Remove redundant query logic for available questions
    - Leverage existing `select_random_question` method for more consistent question distribution

--- lib/questions.rb
+++ lib/questions.rb
@@ -3,14 +3,10 @@ module Questions
     Group.find_each do |group|
       # Only get active users for the group
       active_users = group.active_users
-
       next if active_users.empty?
 
-      # Get a random question that hasn't been used for this group
-      available_questions = group.available_questions
-        .where.not(id: group.recorded_questions.pluck(:id))
-
-      if question = available_questions.sample
+      # Use the refined question selection logic from Question model
+      if question = Question.select_random_question(group)
         # Create question record
         question_record = group.question_records.create!(question: question)
 
